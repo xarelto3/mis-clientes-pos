1755dbbe0be1363455216156c9e680b964a30e2f
xarelto3 <jsilva6458@gmail.com>
2026-05-02 21:19:32 +0200

    Add files via upload

---
+++ main (1).jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PanelClientes from './PanelClientes'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PanelClientes />
+  </React.StrictMode>
+)
